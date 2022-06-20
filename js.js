9dabadcac19ac2c39a58a3ffa5cd46951f206789
Jeffe1233 <72674374+Jeffe1233@users.noreply.github.com>
2022-06-20 15:25:13 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -8,3 +8,20 @@ function toggleMenu(event){
 
 btn.addEventListener('click', toggleMenu);
 btn.addEventListener('touchstart', toggleMenu);
+
+var contador = 1;
+var img1="skol.png";
+var img2="Brahama.png";
+var img3="Heiniken.png";
+var img4="Corona.png";
+var tempo=2000;
+var roda_slide=setInterval("mostra()", tempo);
+
+function mostra()
+{
+    document.images["slide"].src=eval("img"+contador);
+    if(contador <4)
+    contador++;
+    else
+    contador=1;
+}
